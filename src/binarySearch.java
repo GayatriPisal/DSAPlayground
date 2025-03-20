4b2c963ee9f325cda353f8c1362d5d74255953a6
GayatriPisal <gayatripisal93@gmail.com>
2025-03-20 07:37:48 -0700

    Initial commit

---
+++ src/binarySearch.java
@@ -0,0 +1,77 @@
+//import java.util.Arrays;
+
+public class binarySearch {
+
+    public static void main(String[] args)
+    {
+        int[] arr = {0,3,6,8,10,12,20,34,45,55};
+
+        int target = 20;
+
+        //using inbuilt function. This function provides the indx value of the target.
+        //If target is not present in the array then it provides the possible insertion index: -(insertion index)-1
+        //if target is 11 then it needs to be between 10,12 so -(5)-1= -6
+
+        //int indx= Arrays.binarySearch(arr,target);
+
+        //Iterative approach
+        int indx= binarySearch(arr, target);
+
+        System.out.println("Indx :" + indx + " target :"+target);
+
+        if(indx <0)
+            System.out.println("Not found");
+        else
+            System.out.println("Found");
+
+        //Recursive approach
+        int idx= recBinarySearch(arr, target,0, arr.length-1);
+
+        System.out.println("Idx :" + idx + " target :"+target);
+
+        if(indx <0)
+            System.out.println("Not found");
+        else
+            System.out.println("Found");
+
+    }
+
+    public static int binarySearch(int[] arr, int target)
+    {
+        //Iterative approach
+        int start=0;
+        int end = arr.length-1;
+
+        while(start <= end)
+        {
+            int mid = start + (end-start)/2; //this takes care of int value limit overflow
+
+            if(target > arr[mid])
+                start = mid+1;
+            else if(target < arr[mid])
+                end = mid-1;
+            else
+                return mid;
+
+        }
+        return -1;
+
+    }
+
+    public static int recBinarySearch(int[] arr, int target, int start, int end)
+    {
+        if(start < end)
+        {
+            int mid = start + (end-start/2);
+
+            if(target > arr[mid])
+                return recBinarySearch(arr,target, mid+1, end);  //check right of mid
+            else if(target < arr[mid])
+                return recBinarySearch(arr,target, start, mid-1); //check left of mid
+            else
+                return mid; //found
+
+        }
+        return -1;
+    }
+}
---
+++ src/mergeSort.java
@@ -0,0 +1,82 @@
+public class mergeSort {
+
+    public static void main(String[] args)
+    {
+        int arr[] = {10,16,8,12,15,6,3,9,5};
+
+        System.out.println("Array : ");
+        for(int n : arr)
+            System.out.print(n + " ");
+
+        mergeSort(arr, 0, arr.length-1);
+
+        System.out.println("\nSorted Array : ");
+        for(int n : arr)
+            System.out.print(n + " ");
+
+    }
+
+    public static void mergeSort(int[] arr, int start, int end)
+    {
+        if(start < end)
+        {
+            //int mid = (start+end)/2;
+
+            int mid = start + (end-start)/2;
+
+            mergeSort(arr, start, mid);
+            mergeSort(arr, mid+1, end);
+
+            merge(arr,start,mid,end);
+        }
+    }
+
+    private static void merge(int[] arr, int start, int mid, int end) {
+
+        int n1 = mid-start+1;
+        int n2= end-mid;
+
+        int lArr[] = new int[n1];
+        int rArr[] = new int[n2];
+
+        for(int i= 0; i < n1; i++ )
+            lArr[i] = arr[start + i];
+
+        for(int i= 0; i < n2; i++)
+            rArr[i] = arr[mid+1+i];
+
+        int i=0;  //pointers for left, right and main array
+        int j=0;
+        int k=start;
+
+        while(i < n1 && j < n2)
+        {
+            if(lArr[i] <= rArr[j])
+            {
+                arr[k] = lArr[i];
+                i++;
+            }
+            else{
+                arr[k] = rArr[j];
+                j++;
+            }
+            k++;
+        }
+
+        //if any array is done then check if another array has elements that can be copied to main array as it is
+        while(i < n1)
+        {
+            arr[k]=lArr[i];
+            i++;
+            k++;
+        }
+
+        while(j < n2)
+        {
+            arr[k]= rArr[j];
+            j++;
+            k++;
+        }
+
+    }
+}
