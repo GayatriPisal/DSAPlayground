b4c4f6611ae2b4a9c51b634c6ce9389312293f08
GayatriPisal <gayatripisal93@gmail.com>
2025-07-30 07:18:40 -0700

    LC longest sub string without repeat characters

---
+++ src/leetcode/longestSubStringWithoutRepeatChar.java
@@ -0,0 +1,40 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class longestSubStringWithoutRepeatChar {
+
+    public static void main(String args[])
+    {
+        String s= "abcabcbb";
+
+        System.out.println(longestSubStringWithoutRepeatChar(s));
+    }
+
+    public static int longestSubStringWithoutRepeatChar(String s)
+    {
+        if(s.length() <2)
+            return s.length();
+
+        HashSet<Character> charSet = new HashSet<>();
+
+        int max=0;
+        int right =0;
+        int left=0;
+
+        while(right < s.length())
+        {
+            while(charSet.contains(s.charAt(right)))
+            {
+                charSet.remove(s.charAt(left));
+                left++;
+            }
+            charSet.add(s.charAt(right));
+            right++;
+
+            max= Math.max(max, (right-left));
+        }
+        return max;
+    }
+}
