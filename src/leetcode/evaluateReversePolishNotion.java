ff191c12a77bba18fd1e886732d7e288696b8750
GayatriPisal <gayatripisal93@gmail.com>
2025-10-05 14:45:33 -0700

    Evaluate Reverse Polish Notion

--- src/leetcode/evaluateReversePolishNotion.java
+++ src/leetcode/evaluateReversePolishNotion.java
@@ -1,10 +1,10 @@
 package leetcode;
 
-import java.util.Arrays;
 import java.util.Stack;
 
 public class evaluateReversePolishNotion {
 
+    //Arrays, Stack
     public static void main(String args[])
     {
         String[] s = {"2","1","+","3","*"};
